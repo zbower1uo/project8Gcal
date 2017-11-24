70c8cb0c50c22771065c1d3c493071ff9c85118c
pork3 <zbower1@gmail.com>
2017-11-23 23:32:16 -0800

    l

--- test_time.py
+++ test_time.py
@@ -4,7 +4,10 @@ import arrow
 test = arrow.utcnow()
 arrowobj = arrow.get('2013-05-11T21:23:58.970460+00:00')
 
-
+def create_timeblock(arrowo):
+	t1 = arrowo.shift(days=+1)
+	tb = timeblock.TimeBlock(arrowo, t1 , "Test")
+	return tb
 
 def match(input , expected):
 	if input == expected:
@@ -21,4 +24,9 @@ def test_obj_functions():
 	arrowobjdate = arrowobj.date()
 	tb = timeblock.TimeBlock(arrowobj)
 	return match(tb._startdate ,arrowobjdate)
+
+def test_obj_eq():
+	tb1 = create_timeblock(arrowobj)
+	tb2 = create_timeblock(arrowobj)
+	return match(tb1 , tb2)
 
