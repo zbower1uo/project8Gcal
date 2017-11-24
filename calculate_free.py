3975c7b99445e9e94d44b68c93948354e31b8802
pork3 <zbower1@gmail.com>
2017-11-23 22:49:33 -0800

    just have to test edge cases

--- calculate_free.py
+++ calculate_free.py
@@ -4,13 +4,7 @@ import logging
 import arrow
 
 def freetimes(busylist, begin, end):
-  '''
-Current version for creating free times takes begin times and end times,
-creates a list of free times then parses it and returns the parsed times
-  '''
   busyList = []
-  for b in busylist:
-    print(b)
   for event in busylist:
   	e = timeblock.TimeBlock(event._start, event._end, event._summary)
   	busyList.append(e)
@@ -18,33 +12,22 @@ creates a list of free times then parses it and returns the parsed times
   freeTimes = create_free_time(begin, end)
   #print(freeTimes)
   freeBlocks = []
-  for etime in busyList:
-    for ft in freeTimes:
-      #dates are equal
-      if ft._startdate == etime._startdate or ft._enddate == etime._enddate: # if dates are the same
-        if etime._starttime < ft._starttime and etime._endtime > ft._endtime:
-          continue
-        if etime._starttime >= ft._endtime or etime._endtime <= ft._starttime:
+  for ft in freeTimes:
+    if len(busyList) > 0:
+      for ev in busyList:
+        if ft._startdate == ev._startdate or ft._enddate == ev._enddate:
+              if ev._starttime >= ft._starttime and ev._endtime <= ft._endtime:
+                timeblock1 , timeblock2 = ft.split_block(ev)
+                freeBlocks.append(timeblock1)
+                freeBlocks.append(timeblock2)
+        else:
           if ft not in freeBlocks:
             freeBlocks.append(ft)
-        else:
-          timeblock1 , timeblock2 = ft.split_block(etime)
-          if timeblock1._starttime >= timeblock1._endtime and timeblock2._starttime >= timeblock2._endtime:
-            continue
-          if timeblock2._starttime >= timeblock2._endtime:
-            freeBlocks.append(timeblock1)
-          if timeblock1._starttime >= timeblock1._endtime:
-            freeBlocks.append(timeblock2)
-          elif timeblock1._starttime < timeblock1._endtime and timeblock2._starttime < timeblock2._endtime:
-            freeBlocks.append(timeblock1)
-            freeBlocks.append(timeblock2)
-      else:
-        if ft in freeBlocks:
-          continue
-        else:
-          freeBlocks.append(ft)
+    else:
+      freeBlocks.append(ft)
 
   return freeBlocks
+  
 
 
 def create_free_time(begin , end):
